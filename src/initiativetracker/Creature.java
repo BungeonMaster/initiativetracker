c5ef2cf8c2879f3bbd6c8a445044b86d88f1f578
bunge <bunge@DESKTOP-01P2C2U>
2019-03-29 17:02:14 -0700

    Adding and removing labels

--- src/initiativetracker/Creature.java
+++ src/initiativetracker/Creature.java
@@ -1,14 +1,25 @@
 package initiativetracker;
 
-public class Creature {
+import javax.swing.JLabel;
+
+public class Creature implements Comparable<Creature> {
 	
+	private int initiative;
 	private String name;
 	private int hitPoints;
-	private int initiative;
+	private int armorClass;
+	private JLabel creatureLabel;
 	
-	public Creature(String name, int hitPoints, int initiative) {
+	public Creature( int initiative, String name, int hitPoints, int armorClass) {
+		
+		this.initiative = initiative;
 		this.name = name;
 		this.hitPoints = hitPoints;
+		this.armorClass = armorClass;
+		this.creatureLabel = new JLabel(this.toString());
+	}
+	
+	public void setInitiative(int initiative) {
 		this.initiative = initiative;
 	}
 	
@@ -20,8 +31,16 @@ public class Creature {
 		this.hitPoints = hitPoints;
 	}
 	
-	public void setInitiative(int initiative) {
-		this.initiative = initiative;
+	public void setArmorClass(int armorClass) {
+		this.armorClass = armorClass;
+	}
+	
+	public void setCreatureLabel(JLabel creatureLabel) {
+		this.creatureLabel = creatureLabel;
+	}
+	
+	public int getInitiative() {
+		return this.initiative;
 	}
 	
 	public String getName() {
@@ -32,7 +51,23 @@ public class Creature {
 		return this.hitPoints;
 	}
 	
-	public int getInitiative() {
-		return this.initiative;
+	public int getArmorClass() {
+		return this.armorClass;
 	}
+	
+	public JLabel getCreatureLabel() {
+		return this.creatureLabel;
+	}
+
+	@Override
+	public int compareTo(Creature otherCreature) {
+		return (this.getInitiative() < otherCreature.getInitiative() ? -1 : 
+            (this.getInitiative() == otherCreature.getInitiative() ? 0 : 1));
+	}
+	
+	 @Override     
+	 public String toString() {         
+	    return " Initiative: " + this.initiative + ", Name: " + this.name + 
+	    		", Hit Points:" + this.hitPoints + ", Armor Class:" + this.armorClass;
+	 }
 }
--- src/initiativetracker/InitiativeTracker.java
+++ src/initiativetracker/InitiativeTracker.java
@@ -2,6 +2,9 @@ package initiativetracker;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import javax.swing.*;
 
@@ -9,28 +12,94 @@ public class InitiativeTracker extends JFrame implements ActionListener {
 	
 	private static final long serialVersionUID = 1L;
 	
-	JPanel panel;
-	JLabel label;
+	JButton addButton, sortButton, advanceButton;
+	
+	ArrayList<Creature> initiativeList;
+	
+	int verticalFrameSize;
 	
 	public InitiativeTracker() {
 		super("Initiative Tracker");
-		setSize(150,100);
+		verticalFrameSize = 150;
+		setSize(200,verticalFrameSize);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLayout( new FlowLayout() );       // set the layout manager
-	    label = new JLabel("Hello Swing!");  // construct a JLabel
-	    add( label ); 
+		setLayout( new FlowLayout() );       
+		
+	    addButton = new JButton("Add Creature"); 
+	    sortButton = new JButton("Sort Initiative"); 
+	    advanceButton = new JButton("Advance Initiative"); 
+	    
+	    addButton.addActionListener(this);
+	    sortButton.addActionListener(this);
+	    advanceButton.addActionListener(this);
+	    
+	    add(addButton);
+	    add(sortButton);
+	    add(advanceButton);
+	    
 	    setVisible(true);
+	    
+	    initiativeList = new ArrayList<Creature>();
 	}
 
 	public static void main(String[] args) {
 		InitiativeTracker tracker = new InitiativeTracker();
-		
-		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		if(e.getActionCommand().equals("Add Creature")) {
+			System.out.println("Adding Creature");
+			verticalFrameSize += 10;
+			setSize(200, verticalFrameSize);
 		
+			addCreature();
+			
+			for(int i = 0; i < initiativeList.size();i++) {
+				add(initiativeList.get(i).getCreatureLabel());
+			}
+			
+			revalidate();
+			repaint();
+		}
+		else if(e.getActionCommand().equals("Sort Initiative")) {
+			System.out.println("Sorting Initiative");
+			
+			for(int i = 0; i < initiativeList.size();i++) {
+				remove(initiativeList.get(i).getCreatureLabel());
+				System.out.print(initiativeList.get(i).getName());
+			}
+			
+			revalidate();
+			repaint();
+		}
+		else if(e.getActionCommand().equals("Advance Initiative")) {
+			System.out.println("Advancing Initiative");
+		}
+	}
+	
+	private void addCreature() {
+		
+		JTextField initiative = new JTextField();
+		JTextField name = new JTextField();
+		JTextField hitPoints = new JTextField();
+		JTextField armorClass = new JTextField();
+		
+		Object[] message = {
+			"Initiative:", initiative,
+			"Name:", name,
+			"Hit Points:", hitPoints,
+			"Armor Class:", armorClass
+		};
+		
+		int option = JOptionPane.showConfirmDialog(null, message, "Add Creature", JOptionPane.OK_CANCEL_OPTION);
+		
+		if (option == JOptionPane.OK_OPTION) {
+			initiativeList.add(new Creature(
+					Integer.parseInt(initiative.getText()),
+					name.getText(),
+					Integer.parseInt(hitPoints.getText()),
+					Integer.parseInt(armorClass.getText())));
+		}
 	}
 }
